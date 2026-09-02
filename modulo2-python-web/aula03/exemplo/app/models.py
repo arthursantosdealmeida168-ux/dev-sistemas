47da6c05f42bdfa75d0b70c1a0bf94f88c94c96e
arthursantosdealmeida168-ux <arthursantosdealmeida168@gmail.com>
2026-09-02 17:34:38 -0300

    arrumado projeto

--- modulo2-python-web/aula03/exemplo/app/models.py
+++ modulo2-python-web/aula03/exemplo/app/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, ConfigDict
 from typing import Optional
 from enum import Enum
 
@@ -11,7 +11,16 @@ class CargoEnum(str, Enum):
     product_manager = 'Product Manager'
 
 # Schema de entrada: o que o cliente envia (SEM id)
-class UsuariEntrada (BaseModel):
+class UsuarioEntrada (BaseModel):
+    model_config = ConfigDict(
+        json_schema_extra={'example':{
+            'nome': 'Max Muller',
+            'email': 'max@email.com',
+            'cargo': 'Dev',
+            'ativo': True,
+            'salario': 4500.0
+        }}
+    )
     nome: str
     email: str
     cargo: str
@@ -34,4 +43,12 @@ class UsuarioSaida(BaseModel):
     cargo: CargoEnum
     ativo: bool
     salario: Optional [float] = None
-        
+
+# Schema para atualização parcial (PATCH)
+# Todos os campos são Optional - o cliente envia só o que quer mudar
+class UsuarioParcial(BaseModel):
+    nome: Optional[str] = None
+    email: Optional[str] = None
+    cargo: Optional[CargoEnum] = None
+    ativo: Optional[bool] = None
+    salario: Optional[float] = None
